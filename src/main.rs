a969a5a029de87c4dedfffae4dbd9dc849c7728a
Gagan Kumar <gagan1kumar.cs@gmail.com>
2022-03-20 20:33:45 +0530

    Shuffle tiles at init

--- src/main.rs
+++ src/main.rs
@@ -4,72 +4,74 @@ use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use std::io::Write;
+use rand::Rng;
 
 const ROWS:usize = 5;
 const COLS:usize = 5;
 
 const STATE_CELL_EMPTY:u32 = 0;
 
-trait Game {
-    fn init(&mut self);
-    fn get_empty_cell(&self) -> (usize, usize);
-    fn move_up(&mut self);
-    fn move_left(&mut self);
-    fn move_right(&mut self);
-    fn move_down(&mut self);
-    fn draw(&self, stdout: &mut termion::raw::RawTerminal<std::io::Stdout>);
-    fn step(&mut self);
-}
-
 struct SlidingPuzzle {
     state: [[u32; COLS]; ROWS],
+    empty_cell: (usize, usize),
 }
 
-impl Game for SlidingPuzzle {
-    fn init(&mut self) {
+impl SlidingPuzzle {
+    fn new(&mut self) {
         for i in 0..ROWS {
             for j in 0..COLS {
                 self.state[i][j] = (i*COLS + j + 1) as u32;
             }
         }
         self.state[ROWS-1][COLS-1] = 0;
+        self.empty_cell = (ROWS-1, COLS-1);
+
+        let shuffle_count = ROWS*COLS*ROWS*COLS;
+        self.shuffle(shuffle_count);
     }
-    fn get_empty_cell(&self) -> (usize, usize) {
-        for i in 0..ROWS {
-            for j in 0..COLS {
-                if self.state[i][j] == STATE_CELL_EMPTY {
-                    return (i, j);
-                }
+    fn shuffle(&mut self, count: usize) {
+        let mut rng = rand::thread_rng();
+        for _ in 0..count {
+            let num: u32 = rng.gen();
+            match num%4 {
+                0 => self.move_up(),
+                1 => self.move_left(),
+                2 => self.move_right(),
+                3 => self.move_down(),
+                _ => (),
             }
         }
-        panic!("no empty cell found!");
     }
     fn move_up(&mut self) {
-        let (empty_r, empty_c) = self.get_empty_cell();
+        let (empty_r, empty_c) = self.empty_cell;
         if empty_r+1 < ROWS {
             self.state[empty_r][empty_c] = self.state[empty_r+1][empty_c];
             self.state[empty_r+1][empty_c] = STATE_CELL_EMPTY;
+            self.empty_cell.0+=1;
         }
     }
     fn move_left(&mut self) {
-        let (empty_r, empty_c) = self.get_empty_cell();
+        let (empty_r, empty_c) = self.empty_cell;
         if empty_c+1 < COLS {
             self.state[empty_r][empty_c] = self.state[empty_r][empty_c+1];
             self.state[empty_r][empty_c+1] = STATE_CELL_EMPTY;
+            self.empty_cell.1+=1;
         }
     }
     fn move_right(&mut self) {
-        let (empty_r, empty_c) = self.get_empty_cell();
+        let (empty_r, empty_c) = self.empty_cell;
         if empty_c > 0 {
             self.state[empty_r][empty_c] = self.state[empty_r][empty_c-1];
             self.state[empty_r][empty_c-1] = STATE_CELL_EMPTY;
+            self.empty_cell.1-=1;
         }
     }
     fn move_down(&mut self) {
-        let (empty_r, empty_c) = self.get_empty_cell();
+        let (empty_r, empty_c) = self.empty_cell;
         if empty_r > 0 {
             self.state[empty_r][empty_c] = self.state[empty_r-1][empty_c];
             self.state[empty_r-1][empty_c] = STATE_CELL_EMPTY;
+            self.empty_cell.0-=1;
         }
     }
     fn draw(&self, stdout: &mut termion::raw::RawTerminal<std::io::Stdout>) {
@@ -115,18 +117,17 @@ impl Game for SlidingPuzzle {
 
         stdout.flush().unwrap();
     }
-
-    fn step(&mut self) {
-
-    }
 }
 
 fn main() {
     let mut stdout = std::io::stdout().into_raw_mode().unwrap();
     let stdin = std::io::stdin();
 
-    let mut game = SlidingPuzzle{state: [[0u32; COLS as usize]; ROWS as usize]};
-    game.init();
+    let mut game = SlidingPuzzle{
+        state: [[0u32; COLS as usize]; ROWS as usize],
+        empty_cell: (0, 0),
+    };
+    game.new();
     game.draw(&mut stdout);
 
     for c in stdin.keys() {
